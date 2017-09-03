3fb4b05efd0f00fe151d0c57659f6ff18a4771c2
Tim Niven <tim.niven.public@outlook.com>
2017-09-03 21:48:42 +0800

    Added glovar paths.

--- glovar.py
+++ glovar.py
@@ -1,6 +1,6 @@
 """Global variables."""
 
 
-DATA_DIR = ''
-PKL_DIR = ''
-GLOVE_DIR = ''
+DATA_DIR = 'home/hanshan/dev/data/'
+PKL_DIR = 'home/hanshan/dev/cstlstm/pickles/'
+GLOVE_DIR = 'home/hanshan/dev/data/glove.840B.300d.txt'
